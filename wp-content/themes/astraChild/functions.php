644f71f3c1bc7cb6153f7c7a1ae383c39b00d66d
Gaëtan Brun <brungaetan@outlook.fr>
2023-08-02 15:30:30 +0200

    Modification du Hook (prenant la bonne place dans toutes les pages)

--- wp-content/themes/astraChild/functions.php
+++ wp-content/themes/astraChild/functions.php
@@ -12,22 +12,44 @@ function theme_enqueue_styles()
 }
 
 function mon_hook_menu( $items, $args ) {
-    // Vérifier si l'utilisateur est connecté et si le menu est le menu principal (location = 'primary')
-    if ( is_user_logged_in() && $args->theme_location === 'primary'|| $args->theme_location === 'mobile_menu' ) {
+    // Vérifier si l'utilisateur est connecté & si le menu est le menu principal (location = 'primary') & si le menu est le menu mobile (location = 'mobile_menu')
+    /* if ( is_user_logged_in() && $args->theme_location === 'primary'|| $args->theme_location === 'mobile_menu' ) {
+
         // Lien vers la page d'administration
         $admin_link = '<li class="menu-item admin-link"><a href="' . admin_url() . '">Admin</a></li>';
+        debug_to_console("1= " . $admin_link);
 
         // Trouver l'emplacement où insérer le lien "Admin"
         $position = strpos( $items, '</li>', ceil( strlen( $items ) / 2 ) );
-
+        debug_to_console("2= " . $position);
         // Insérer le lien "Admin" à l'emplacement trouvé
         if ( $position !== false ) {
             $items = substr_replace( $items, $admin_link, $position, 0 );
+        debug_to_console("3= " . $position);
+        }
+   }
+    debug_to_console("4= " . $items);
+    return $items;
+} */
+    // Vérifier si l'utilisateur est connecté & si le menu est le menu principal (location = 'primary') & si le menu est le menu mobile (location = 'mobile_menu')
+    if ( is_user_logged_in() && ( $args->theme_location === 'primary' || $args->theme_location === 'mobile_menu' ) ) {
+
+        // Lien vers la page d'administration
+        $admin_link = '<li class="menu-item admin-link"><a href="' . admin_url() . '">Admin</a></li>';
+
+        // Trouver la première occurrence de la balise '</li>'
+        $position = strpos( $items, '</li>' );
+
+        // Insérer le lien "Admin" après la première occurrence trouvée
+        if ( $position !== false ) {
+            $items = substr_replace( $items, $admin_link, $position + 5, 0 );
         }
     }
 
     return $items;
+
 }
+
 add_filter( 'wp_nav_menu_items', 'mon_hook_menu', 10, 2 );
 
 add_filter( 'astra_off_canvas_menu_content', 'mon_hook_menu', 10, 2 );
@@ -39,3 +61,12 @@ function mon_afficher_emplacements_menu() {
     print_r( $registered_menus );
     echo '</pre>';
 } */
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
